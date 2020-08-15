7a3432b939f1a980359493a40f89dd848f293f3d
Elina <elisonfog@gmail.com>
2020-08-15 13:30:08 +0300

    added task

---
+++ script.js
@@ -0,0 +1,34 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector("body");
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function colorSwitcher() {
+  let selectedColor = colors[`${randomIntegerFromInterval(0, colors.length)}`];
+  body.style.backgroundColor = selectedColor;
+  startBtn.setAttribute("disabled", "disabled");
+}
+
+let intervalSwitcher = null;
+
+startBtn.addEventListener("click", () => {
+  intervalSwitcher = setInterval(() => {
+    colorSwitcher();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalSwitcher), startBtn.removeAttribute("disabled");
+});
